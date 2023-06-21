99af133948371212e34f9508aac21f23e041a536
Adelina Cazacu <a.a.cazacu-1@student.tudelft.nl>
2023-06-21 13:04:59 +0200

    Testsets for everything besides KFAC in testutils.jl

---
+++ test/pytorch_comparison.jl
@@ -0,0 +1,151 @@
+using LaplaceRedux
+using CSV
+using Test
+using Flux
+using Serialization
+using DataFrames
+include("testutils.jl")
+
+# This test suite compares results for the hessian between the current Julia package and the corresponding Laplace from PyTorch as reference.
+# https://github.com/AlexImmer/Laplace
+
+@testset "Pytorch Hessian Comparisons" begin
+
+    @testset "Multi-Class Classification" begin
+
+        # Read the dataset from a CSV file
+        df = CSV.read(joinpath(@__DIR__, "datafiles\\data_multi.csv"), DataFrame)
+        x = Matrix(df[:, 1:2])
+        x = [x[i, :] for i in 1:size(x, 1)]
+        y = df[:, 3]
+
+        X = hcat(x...)
+        y_train = Flux.onehotbatch(y, unique(y))
+        y_train = Flux.unstack(y_train',1)
+
+        data = zip(x,y_train)
+
+
+        # Read the network weights and biases from a JLB file
+        nn = deserialize(joinpath(@__DIR__, "datafiles\\nn-binary_multi.jlb"))
+
+
+        @testset "LA - full weights - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:all,
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_all_full_ggn")
+            @test isapprox(pytorch_hessian, rearrangeHessian(la.H, nn); atol = 0.0001)
+        end
+
+        @testset "LA - full weights - full hessian - empfisher" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:all,
+                        backend=:EmpiricalFisher)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_all_full_empfisher")
+            @test isapprox(pytorch_hessian, rearrangeHessian(la.H, nn); atol = 0.0001)
+        end
+
+        @testset "LA - last layer - full hessian - empfisher" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:last_layer,
+                        backend=:EmpiricalFisher)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_ll_full_empfisher")
+            @test isapprox(pytorch_hessian, rearrangeHessianLastLayer(la.H, nn); atol = 0.0005)
+        end
+
+        @testset "LA - last layer - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:last_layer,
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_ll_full_ggn")
+            @test isapprox(pytorch_hessian, rearrangeHessianLastLayer(la.H, nn); atol = 0.0005)
+        end
+
+        @testset "LA - subnetwork - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:subnetwork,
+                        subnetwork_indices=[[1, 1, 1], [1, 2, 1], [1, 3, 1], [1, 3, 2], [2, 1], [2, 3]],
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_subnet_full_ggn")
+            @test isapprox(pytorch_hessian, rearrangeHessian(la.H, nn); atol = 0.0001)
+        end
+
+        @testset "LA - subnetwork - full hessian - empfisher" begin
+            la = Laplace(nn;
+                        likelihood=:classification,
+                        hessian_structure=:full,
+                        subset_of_weights=:subnetwork,
+                        subnetwork_indices=[[1, 1, 1], [1, 2, 1], [1, 3, 1], [1, 3, 2], [2, 1], [2, 3]],
+                        backend=:EmpiricalFisher)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_multi_subnet_full_empfisher")
+            @test isapprox(pytorch_hessian, rearrangeHessian(la.H, nn); atol = 0.0001)
+        end
+    end
+
+    @testset "Regression" begin
+        
+        # Read the dataset from a CSV file
+        df = CSV.read(joinpath(@__DIR__, "datafiles\\data_regression.csv"), DataFrame)
+        x = df[:, 1]
+        y = df[:, 2]
+
+        xs = [[x] for x in x]
+
+        data = zip(xs, y)
+
+
+        # Read the network weights and biases from a JLB file
+        nn = deserialize(joinpath(@__DIR__, "datafiles\\nn-binary_regression.jlb"))
+
+        @testset "LA - full weights - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:regression,
+                        hessian_structure=:full,
+                        subset_of_weights=:all,
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_regression_all_full_ggn")
+            @test isapprox(pytorch_hessian, la.H; atol = 0.05)
+        end
+
+        @testset "LA - last layer - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:regression,
+                        hessian_structure=:full,
+                        subset_of_weights=:last_layer,
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_regression_ll_full_ggn")
+            @test isapprox(pytorch_hessian, la.H; atol = 0.0005)
+        end
+
+        @testset "LA - subnetwork - full hessian - ggn" begin
+            la = Laplace(nn;
+                        likelihood=:regression,
+                        hessian_structure=:full,
+                        subset_of_weights=:subnetwork,
+                        subnetwork_indices=[[1, 2, 1], [1, 4, 1], [1, 6, 1], [1, 7, 1], [1, 8, 1], [1, 10, 1]],
+                        backend=:GGN)
+            fit!(la, data)
+            pytorch_hessian = readHessianCSV("hessian_regression_subnet_full_ggn")
+            @test isapprox(pytorch_hessian, la.H; atol = 0.01)
+        end
+    end
+end
---
+++ test/testutils.jl
@@ -0,0 +1,76 @@
+using Flux, Plots, Random, Statistics, LaplaceRedux
+using Flux.Optimise: update!, Adam
+using CSV
+using DataFrames
+using JSON
+using Serialization
+using Tullio
+using LinearAlgebra
+using Zygote
+using DelimitedFiles
+
+Random.seed!(42)
+
+function readHessianCSV(filename::String)
+    # Specify the file path relative to the current directory
+    file_path = joinpath(@__DIR__, "datafiles\\" * filename * ".csv")
+
+    # Read the file using readdlm and create a Matrix{Float64}
+    matrix_data = readdlm(file_path, ',', Float64)
+
+    # Convert the matrix to a regular Julia matrix type
+    matrix = Matrix{Float64}(matrix_data)
+
+    return matrix
+end
+
+function rearrangeHessian(h::Matrix{Float64}, nn::Chain)
+
+    to_row_order(h, nn) = h[gen_mapping_sq(Flux.params(nn))]
+
+    return to_row_order(h, nn)
+end
+
+function rearrangeHessianLastLayer(h::Matrix{Float64}, nn::Chain)
+    ps = [p for p in Flux.params(nn)]
+    M = length(ps[end])
+    N = length(ps[end-1])
+    return h[:, end-M-N:end][gen_mapping_sq(ps[end-1:end])]
+end
+
+function gen_mapping_sq(params)::Array{Tuple{Int64, Int64}}
+    mapping_lin = gen_mapping(params)
+    length_theta = sum(length, params)
+    mapping_sq = Array{Tuple{Int64, Int64}}(undef, length_theta, length_theta)
+    for (i, i_) in enumerate(mapping_lin)
+        for (j, j_) in enumerate(mapping_lin)
+            mapping_sq[i, j] = (i_, j_)
+        end
+    end
+    return mapping_sq
+end
+
+import Base: getindex
+
+function getindex(r::Matrix{Float64}, I::Matrix{Tuple{Int64, Int64}})
+    l = Matrix{Float64}(undef, size(I))
+    for (i, j) in Iterators.product(1:size(I, 1), 1:size(I, 2))
+        # Unpack 2d index
+        x, y = I[i, j]
+        l[i, j] = r[x, y]
+    end
+    return l
+end
+
+function gen_mapping(params)
+    theta_length = sum(length, params)
+    offset = 0
+    mapping = []
+    for param in params
+        indices = collect(1:length(param))
+        indices_updated = vec(reshape(offset .+ indices, size(param))')
+        append!(mapping, indices_updated)
+        offset += length(param)
+    end
+    mapping
+end
