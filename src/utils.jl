733312a869fe28f87e2187d2f9cfe8a10e1ded72
pasquale c. <343guiltyspark@outlook.it>
2024-06-09 05:49:42 +0200

    function empirical_frequency

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,5 @@
 using Flux
+using Statistics
 
 """
     get_loss_fun(likelihood::Symbol)
@@ -38,4 +39,28 @@ corresponding to the number of neurons on the last layer of the NN.
 """
 function outdim(model::Chain)::Number
     return [size(p) for p in Flux.params(model)][end][1]
+end
+
+""" 
+    count_fractions(Y_val,array)
+
+Given a calibration dataset (x_t, y_t) for i ∈ {1,...,T} and an array of predicted distributions, the function calculates the empirical frequency
+phat_j = {y_t|F_t(y_t)<= p_j, t= 1,....,T}/T, where T is the number of calibration points, p_j is the confidence level and F_t is the 
+cumulative density function of the predicted distribution targeting y_t. The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
+    Arguments:
+    Y_val: a vector of values
+    array: an array of sampled distributions stacked column-wise.
+"""
+function empirical_frequency(Y_cal,array)
+
+
+    quantiles= collect(0:0.05:1)
+    quantiles_matrix = hcat([quantile(samples, quantiles) for samples in array]...)
+    n_rows  = size(bounds_quantiles_matrix,1)
+    counts = []
+
+    for i in  1:n_rows
+        push!(counts, sum(Y_cal .<= quantiles_matrix[i, :]) / length(Y_cal))
+    end
+    return counts
 end
