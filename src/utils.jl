0bba48856c381f4495cf238927927c9cc1f0197c
pasquale c. <343guiltyspark@outlook.it>
2024-06-23 23:39:51 +0200

    fixed docstrings

--- src/utils.jl
+++ src/utils.jl
@@ -42,15 +42,17 @@ function outdim(model::Chain)::Number
 end
 
 """ 
-    empirical_frequency(Y_val,array)
+    empirical_frequency(Y_cal, sampled_distributions)
 
 FOR REGRESSION MODELS.
 Given a calibration dataset (x_t, y_t) for i ∈ {1,...,T} and an array of predicted distributions, the function calculates the empirical frequency
 phat_j = {y_t|F_t(y_t)<= p_j, t= 1,....,T}/T, where T is the number of calibration points, p_j is the confidence level and F_t is the 
-cumulative distribution function of the predicted distribution targeting y_t. The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
+cumulative distribution function of the predicted distribution targeting y_t.
+ The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
+
     Arguments:
-    Y_val: a vector of values y_t
-    array: an array of sampled distributions F(x_t) stacked column-wise.
+    -Y_cal: a vector of values y_t
+    -sampled_distributions: an array of sampled distributions F(x_t) stacked column-wise.
 """
 function empirical_frequency(Y_cal, sampled_distributions)
     quantiles = collect(0:0.05:1)
@@ -67,14 +69,15 @@ function empirical_frequency(Y_cal, sampled_distributions)
 end
 
 """ 
-    sharpness(array)
+    sharpness(sampled_distributions)
 
 FOR REGRESSION MODELS.
 Given a calibration dataset (x_t, y_t) for i ∈ {1,...,T} and an array of predicted distributions, the function calculates the 
 sharpness of the predicted distributions, i.e., the average of the variances var(F_t) predicted by the forecaster for each x_t
 The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
+
     Arguments:
-    sampled_distributions: an array of sampled distributions F(x_t) stacked column-wise.
+    -sampled_distributions: an array of sampled distributions F(x_t) stacked column-wise.
 """
 function sharpness(sampled_distributions)
     sharpness = mean(var.(sampled_distributions))
@@ -82,18 +85,19 @@ function sharpness(sampled_distributions)
 end
 
 """ 
-    empirical_frequency-classification(Y_val,array)
+    empirical_frequency-classification(y_binary, sampled_distributions)
 
 FOR BINARY CLASSIFICATION MODELS.
 Given a calibration dataset (x_t, y_t) for i ∈ {1,...,T} let p_t= H(x_t)∈[0,1] be the forecasted probability. 
 We group the p_t into intervals I-j for j= 1,2,...,m that form a partition of [0,1]. The function computes
 the observed average p_j= T^-1_j ∑_{t:p_t ∈ I_j} y_j in each interval I_j. 
 The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
-    Arguments:
-    y_binary: the array of outputs y_t numerically coded . 1 for the target class, 0 for the negative result.
 
-    sampled_distributions: an array of sampled distributions stacked column-wise where in the first row 
-    there is the probability for the target class y_1=1 and in the second row y_0=0.
+    Arguments:
+    -y_binary: the array of outputs y_t numerically coded: 1 for the target class, 0 for the negative result.
+
+    -sampled_distributions: an array of sampled distributions stacked column-wise so that in the first row 
+        there is the probability for the target class y_1 and in the second row the probability for the null class y_0.
 """
 function empirical_frequency_binary_classification(y_binary, sampled_distributions)
     pred_avg = collect(range(0; step=0.1, stop=0.9))
@@ -119,17 +123,18 @@ function empirical_frequency_binary_classification(y_binary, sampled_distributio
 end
 
 """ 
-    sharpness-classification(array)
+    sharpness-classification(y_binary,sampled_distributions)
 
 FOR BINARY CLASSIFICATION MODELS.
 We can also assess sharpness by looking at the distribution of model predictions.When forecasts are sharp, 
-most predictions are close to 0 or 1; unsharp forecasters make predictions closer to 0.5.
+most predictions are close to 0 or 1; not sharp forecasters make predictions closer to 0.5.
 The function was  suggested by Kuleshov(2018) in https://arxiv.org/abs/1807.00263
 
     Arguments:
-    y_binary: the array of outputs y_t numerically coded . 1 for the target class, 0 for the negative result.
-    sampled_distributions: an array of sampled distributions stacked column-wise where in the first row 
-    there is the probability for the target class y_1=1 and in the second row y_0=0.
+
+    -y_binary: the array of outputs y_t numerically coded . 1 for the target class, 0 for the negative result.
+    -sampled_distributions: an array of sampled distributions stacked column-wise so that in the first row 
+        there is the probability for the target class y_1 and in the second row the probability for the null class y_0.
 """
 function sharpness_classification(y_binary, sampled_distributions)
     class_one = sampled_distributions[1, findall(y_binary .== 1)]
