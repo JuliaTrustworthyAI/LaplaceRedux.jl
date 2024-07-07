a94486e2e111cafb99ab9633ca843a6c8e037ff7
pasquale c. <343guiltyspark@outlook.it>
2024-07-07 05:33:15 +0200

    added dispatched functions for calibration. added unit tests. add Trapz to test dependencies. 2 unit tests for the classification case still missing. they are a bit hard. working on the documentations too

--- test/calibration.jl
+++ test/calibration.jl
@@ -1,8 +1,9 @@
 using Statistics
 using LaplaceRedux
 using Distributions
+using Trapz
 
-@testset "sharpness_classification tests" begin
+@testset "sharpness_classification sampled distributions tests" begin
 
     # Test 1: Check that the function runs without errors and returns two scalars for a simple case
     y_binary = [1, 0, 1, 0, 1]
@@ -45,7 +46,7 @@ using Distributions
 end
 
 # Test for `sharpness_regression` function
-@testset "sharpness_regression tests" begin
+@testset "sharpness_regression sampled distributions tests" begin
 
     # Test 1: Check that the function runs without errors and returns a scalar for a simple case
     sampled_distributions = [randn(100) for _ in 1:10]  # Create 10 distributions, each with 100 samples
@@ -67,7 +68,7 @@ end
 end
 
 # Test for `empirical_frequency_regression` function
-@testset "empirical_frequency_regression tests" begin
+@testset "empirical_frequency_regression sampled distributions tests" begin
     # Test 1: Check that the function runs without errors and returns an array for a simple case
     Y_cal = [0.5, 1.5, 2.5, 3.5, 4.5]
     n_bins = 10
@@ -78,6 +79,30 @@ end
 
     # Test 2: Check the function with a known input
     #to do
+        # Step 1: Define the parameters for the sine wave
+        start_point = 0.0  # Start of the interval
+        end_point = 2 * π  # End of the interval, 2π for a full sine wave cycle
+        sample_points = 2000  # Number of sample points between 0 and 2π
+    
+        # Step 2: Generate the sample points
+        x = LinRange(start_point, end_point, sample_points)
+    
+        # Step 3: Generate the sine wave data
+        y = sin.(x)
+        # Step 4: Generate samples
+        distrs= Distributions.Normal.(y,0.01)
+        sampled_distributions = rand.(distrs,100)
+        #fake perfectly calibrated predictions 
+        predicted_elements = rand.(distrs)
+        n_bins=100
+        emp_freq = empirical_frequency_regression(predicted_elements, sampled_distributions; n_bins= n_bins)
+        quantiles = collect(range(0; stop = 1, length = n_bins + 1))
+        area = trapz((quantiles), vec(abs.(emp_freq - quantiles)))
+        @test area < 0.1
+
+
+
+
 
     # Test 3: Invalid n_bins input
     Y_cal = [0.5, 1.5, 2.5, 3.5, 4.5]
@@ -88,7 +113,7 @@ end
 end
 
 # Test for `empirical_frequency_binary_classification` function
-@testset "empirical_frequency_binary_classification tests" begin
+@testset "empirical_frequency_binary_classification sampled distributions tests" begin
     # Test 1: Check that the function runs without errors and returns an array for a simple case
     y_binary = rand(0:1, 10)
     sampled_distributions = rand(2, 10)
@@ -118,3 +143,146 @@ end
         Y_cal, sampled_distributions, n_bins=0
     )
 end
+
+
+
+
+
+# Test for `sharpness_regression` function
+@testset "sharpness_regression distributions tests" begin
+
+    # Test 1: Check that the function runs without errors and returns a scalar for a simple case
+    distributions = [Distributions.Normal.(1, 0.01) for _ in 1:10]  # Create 10 distributions, each with 100 samples
+    sharpness = sharpness_regression(distributions)
+    @test typeof(sharpness) <: Real  # Check if the output is a scalar
+
+    # Test 2: Check the function with a known input
+    distributions = [
+        Distributions.Normal.(1, 0.01), Distributions.Normal.(1, 2), Distributions.Normal.(1, 3)
+    ]
+    mean_variance = mean(map(var, distributions))
+    sharpness = sharpness_regression(distributions)
+    @test sharpness ≈ mean_variance
+
+end
+# Test for `empirical_frequency_regression` function
+@testset "empirical_frequency_regression distributions tests" begin
+    # Test 1: Check that the function runs without errors and returns an array for a simple case
+    Y_cal = [0.5, 1.5, 2.5, 3.5, 4.5]
+    n_bins = 10
+    distributions = [Distributions.Normal(1, 1.0) for _ in 1:5]
+    counts = empirical_frequency_regression(Y_cal, distributions; n_bins=n_bins)
+    @test typeof(counts) == Array{Float64,1}  # Check if the output is an array of Float64
+    @test length(counts) == n_bins + 1
+
+    # Test 2: Check the function with a known input
+
+    # Step 1: Define the parameters for the sine wave
+    start_point = 0.0  # Start of the interval
+    end_point = 2 * π  # End of the interval, 2π for a full sine wave cycle
+    sample_points = 2000  # Number of sample points between 0 and 2π
+
+    # Step 2: Generate the sample points
+    x = LinRange(start_point, end_point, sample_points)
+
+    # Step 3: Generate the sine wave data
+    y = sin.(x)
+    distrs= Distributions.Normal.(y,0.01)
+    #fake perfectly calibrated predictions 
+    predicted_elements = rand.(distrs)
+    n_bins=100
+    emp_freq = empirical_frequency_regression(predicted_elements, distrs; n_bins= n_bins)
+    quantiles = collect(range(0; stop = 1, length = n_bins + 1))
+    area = trapz((quantiles), vec(abs.(emp_freq - quantiles)))
+    @test area < 0.1
+
+
+
+
+    # Test 3: Invalid n_bins input
+    Y_cal = [0.5, 1.5, 2.5, 3.5, 4.5]
+    distributions = [Distributions.Normal(1, 1.0) for _ in 1:5]
+    @test_throws ArgumentError empirical_frequency_regression(
+        Y_cal, distributions, n_bins=0
+    )
+end
+@testset "sharpness_classification distributions tests" begin
+
+    # Test 1: Check that the function runs without errors and returns two scalars for a simple case
+    y_binary = [1, 0, 1, 0]
+    distributions = [Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.7)]  # probabilities
+    mean_class_one, mean_class_zero = sharpness_classification(
+        y_binary, distributions
+    )
+    @test typeof(mean_class_one) <: Real  # Check if mean_class_one is a scalar
+    @test typeof(mean_class_zero) <: Real  # Check if mean_class_zero is a scalar
+
+    # Test 2: Check the function with a known input
+    y_binary = [0, 1, 0, 1, 1]
+    distributions = [
+        Distributions.Bernoulli(0.7)
+        Distributions.Bernoulli(0.7)
+        Distributions.Bernoulli(0.3)
+        Distributions.Bernoulli(0.5)
+        Distributions.Bernoulli(0.9)
+    ]
+    mean_class_one, mean_class_zero = sharpness_classification(
+        y_binary, distributions
+    )
+    @test mean_class_one ≈ mean(mean.(distributions[ [2, 4, 5]]))
+    @test mean_class_zero ≈ mean(mean.(distributions[ [1, 3]]))
+
+    # Test 3: Edge case with all ones in y_binary
+    y_binary_all_ones = [1, 1, 1]
+    distributions_all_ones = [ Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.3)]
+    mean_class_one_all_ones, mean_class_zero_all_ones = sharpness_classification(
+        y_binary_all_ones, distributions_all_ones
+    )
+    @test mean_class_one_all_ones ≈ mean([0.7, 0.7, 0.3])
+    @test isnan(mean_class_zero_all_ones)  # Since there are no zeros in y_binary, the mean should be NaN
+
+    # Test 4: Edge case with all zeros in y_binary
+    y_binary_all_zeros = [0, 0, 0]
+    distributions_all_zeros = [Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.7); Distributions.Bernoulli(0.3)]
+    mean_class_one_all_zeros, mean_class_zero_all_zeros = sharpness_classification(
+        y_binary_all_zeros, distributions_all_zeros
+    )
+    @test mean_class_zero_all_zeros ≈ mean([0.3, 0.3, 0.7])
+    @test isnan(mean_class_one_all_zeros)  # Since there are no ones in y_binary, the mean should be NaN
+end
+
+# Test for `empirical_frequency_binary_classification` function
+@testset "empirical_frequency_binary_classification distributions tests" begin
+    # Test 1: Check that the function runs without errors and returns an array for a simple case
+    y_binary = rand(0:1, 10)
+    distributions = [Distributions.Bernoulli(0.7) for _ in 1:10]
+    n_bins = 4
+    num_p_per_interval, emp_avg, bin_centers = empirical_frequency_binary_classification(
+        y_binary, distributions; n_bins=n_bins
+    )
+    @test length(num_p_per_interval) == n_bins
+    @test length(emp_avg) == n_bins
+    @test length(bin_centers) == n_bins
+
+    # Test 2: Check the function with a known input
+    
+
+    #to do
+
+    # Test 3: Invalid Y_cal input
+    Y_cal = [0, 1, 0, 1.2, 4]
+    distributions = [Distributions.Bernoulli(0.7) for _ in 1:5]
+    @test_throws ArgumentError empirical_frequency_binary_classification(
+        Y_cal, distributions, n_bins=10
+    )
+
+    # Test 4: Invalid n_bins input
+    Y_cal = rand(0:1, 5)
+    distributions = [Distributions.Bernoulli(0.7) for _ in 1:5]
+    @test_throws ArgumentError empirical_frequency_binary_classification(
+        Y_cal, distributions, n_bins=0
+    )
+end
+
+
+
