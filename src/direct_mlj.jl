53152645ebec793d86d37fc1d4e818aaf75f45c4
pasquale c. <343guiltyspark@outlook.it>
2024-09-18 11:03:37 +0200

    almost fixed

--- src/direct_mlj.jl
+++ src/direct_mlj.jl
@@ -15,6 +15,7 @@ It uses Laplace approximation to estimate the posterior distribution of the weig
 It has the following Hyperparameters:
 - `flux_model`: A flux model provided by the user and compatible with the dataset.
 - `epochs`: The number of training epochs.
+- `batch_size`: The batch size.
 - `subset_of_weights`: the subset of weights to use, either `:all`, `:last_layer`, or `:subnetwork`.
 - `subnetwork_indices`: the indices of the subnetworks.
 - `hessian_structure`: the structure of the Hessian matrix, either `:full` or `:diagonal`.
@@ -29,6 +30,7 @@ MLJBase.@mlj_model mutable struct LaplaceRegressor <: MLJFlux.MLJFluxProbabilist
 
     flux_model::Flux.Chain = nothing
     epochs::Integer = 1000::(_ > 0)
+    batch_size::Integer= 32::(_ > 0)
     subset_of_weights::Symbol = :all::(_ in (:all, :last_layer, :subnetwork))
     subnetwork_indices = nothing
     hessian_structure::Union{HessianStructure,Symbol,String} =
@@ -47,7 +49,7 @@ function MMI.fit(m::LaplaceRegressor, verbosity, X, y)
 
     X = MLJBase.matrix(X) |> permutedims
     y = reshape(y, 1,:)
-    data_loader = Flux.DataLoader((X,y), batchsize=10)
+    data_loader = Flux.DataLoader((X,y), batchsize=m.batch_size)
     opt_state = Flux.setup(Adam(), m.flux_model)
     loss(y_hat, y) = mean(Flux.Losses.mse.(y_hat, y))
 
@@ -88,9 +90,13 @@ end
 
 function MMI.predict(m::LaplaceRegressor, fitresult, Xnew)
     Xnew = MLJBase.matrix(Xnew) |> permutedims
-    la = fitresult[1]
-    yhat = LaplaceRedux.predict(la, Xnew; ret_distr=model.ret_distr)
-    return [Normal(μᵢ, σ̂) for (μᵢ,σ) ∈ yhat]
+    la = fitresult
+    yhat = LaplaceRedux.predict(la, Xnew; ret_distr=m.ret_distr)
+    # Extract mean and variance matrices
+    means, variances = yhat
+
+    # Create Normal distributions from the means and variances
+    return [Normal(μ, sqrt(σ)) for (μ, σ) in zip(means, variances)]
 end
 
 
@@ -108,6 +114,7 @@ The model also has the following parameters:
 
 - `flux_model`: A flux model provided by the user and compatible with the dataset.
 - `epochs`: The number of training epochs.
+- `batch_size`: The batch size.
 - `subset_of_weights`: the subset of weights to use, either `:all`, `:last_layer`, or `:subnetwork`.
 - `subnetwork_indices`: the indices of the subnetworks.
 - `hessian_structure`: the structure of the Hessian matrix, either `:full` or `:diagonal`.
@@ -124,33 +131,41 @@ MLJBase.@mlj_model mutable struct LaplaceClassifier <: MLJFlux.MLJFluxProbabilis
 
     flux_model::Flux.Chain = nothing
     epochs::Integer = 1000::(_ > 0)
+    batch_size::Integer= 32::(_ > 0)
     subset_of_weights::Symbol = :all::(_ in (:all, :last_layer, :subnetwork))
-    subnetwork_indices::Vector{Vector{Int}} = Vector{Vector{Int}}([])
+    subnetwork_indices = nothing
     hessian_structure::Union{HessianStructure,Symbol,String} =
         :full::(_ in (:full, :diagonal))
     backend::Symbol = :GGN::(_ in (:GGN, :EmpiricalFisher))
     σ::Float64 = 1.0
     μ₀::Float64 = 0.0
     P₀::Union{AbstractMatrix,UniformScaling,Nothing} = nothing
-    link_approx::Symbol = :probit::(_ in (:probit, :plugin))
-    predict_proba::Bool = true::(_ in (true, false))
     ret_distr::Bool = false::(_ in (true, false))
     fit_prior_nsteps::Int = 100::(_ > 0)
+    link_approx::Symbol = :probit::(_ in (:probit, :plugin))
 end
 
-
+#link_approx::Symbol = :probit::(_ in (:probit, :plugin))
 
 function MMI.fit(m::LaplaceClassifier, verbosity, X, y)
     features  = Tables.schema(X).names
-    Xmatrix = MLJBase.matrix(X)
+    X = MLJBase.matrix(X) |> permutedims
+    y = reshape(y, 1,:)
+
     decode    = y[1]
     y_plain   = MLJBase.int(y) .- 1 # 0, 1 of type Int
 
 
+    loss(y_hat, y) =  mean(Flux.Losses.crossentropy(y_hat, y))
+
+    data_loader = Flux.DataLoader((X,y_plain), batchsize=m.batch_size)
+    opt_state = Flux.setup(Adam(), m.flux_model)
+
+
 
     for epoch in 1:m.epochs
-        Flux.train!(m.flux_model,data_loader, opt_state) do model, X, y
-            loss(model(X), y)
+        Flux.train!(m.flux_model,data_loader, opt_state) do model, X, y_plain
+            loss(model(X), y_plain)
         
         end
       end
@@ -160,10 +175,10 @@ function MMI.fit(m::LaplaceClassifier, verbosity, X, y)
     la = LaplaceRedux.Laplace(
         m.flux_model;
         likelihood=:classification,
-        subset_of_weights=model.subset_of_weights,
-        subnetwork_indices=model.subnetwork_indices,
-        hessian_structure=model.hessian_structure,
-        backend=model.backend,
+        subset_of_weights=m.subset_of_weights,
+        subnetwork_indices=m.subnetwork_indices,
+        hessian_structure=m.hessian_structure,
+        backend=m.backend,
         σ=m.σ,
         μ₀=m.μ₀,
         P₀=m.P₀,
@@ -173,27 +188,27 @@ function MMI.fit(m::LaplaceClassifier, verbosity, X, y)
 
 
     # fit the Laplace model:
-    LaplaceRedux.fit!(la, zip(X, y_plain))
-    optimize_prior!(la; verbose=verbose_laplace, n_steps=model.fit_prior_nsteps)
+    LaplaceRedux.fit!(la, data_loader )
+    optimize_prior!(la; verbose= false, n_steps=m.fit_prior_nsteps)
 
-
+    
+    fitresult=la
     report = (status="success", message="Model fitted successfully")
     cache     = nothing
-    return (la, decode), cache, report
+    return ((fitresult,decode), cache, report)
 end
 
 
 function MMI.predict(m::LaplaceClassifier, (fitresult, decode), Xnew)
-    la = fitresult
+    #la = fitresult
     Xnew = MLJBase.matrix(Xnew) |> permutedims
-    predictions = LaplaceRedux.predict(
-        la,
-        Xnew;
-        link_approx=model.link_approx,
-        predict_proba=model.predict_proba,
-        ret_distr=model.ret_distr,
-    )
-    return [MLJBase.UnivariateFinite(MLJBase.classes(decode), prediction) for prediction in predictions]
+    #predictions = LaplaceRedux.predict(
+        #la,
+        #Xnew;
+        #link_approx=model.link_approx,
+        #predict_proba=model.predict_proba,
+        #ret_distr=model.ret_distr)
+    #return [MLJBase.UnivariateFinite(MLJBase.classes(decode), prediction) for prediction in predictions]
 end
 
 
