c44b8d83795690e32786a13c805c139728acb190
pasquale c. <343guiltyspark@outlook.it>
2024-09-23 18:54:51 +0200

    added tests. should be good....

--- test/direct_mlj_interface.jl
+++ test/direct_mlj_interface.jl
@@ -3,96 +3,49 @@ import Random.seed!
 using MLJBase: MLJBase, categorical
 using Flux
 using StableRNGs
+using MLJ
+using LaplaceRedux
 
 
 @testset "Regression" begin
-    function basictest_regression(X, y, builder, optimiser, threshold)
-        optimiser = deepcopy(optimiser)
+    flux_model = Chain(
+        Dense(4, 10, relu),
+        Dense(10, 10, relu),
+        Dense(10, 1)
+    )
+    model = LaplaceRegressor(model=flux_model,epochs=50)
+    
+    X, y = make_regression(100, 4; noise=0.5, sparse=0.2, outliers=0.1)
+    mach = machine(model, X, y) #|> MLJBase.fit! #|> (fitresult,cache,report)
+    MLJBase.fit!(mach)
+    Xnew, _ = make_regression(3, 4; rng=123)
+    yhat = MLJBase.predict(mach, Xnew) # probabilistic predictions
+    MLJBase.predict_mode(mach, Xnew)   # point predictions
+    MLJBase.fitted_params(mach)   #fitted params function 
+end
 
-        stable_rng = StableRNGs.StableRNG(123)
 
-        model = LaplaceRegression(;
-            builder=builder,
-            optimiser=optimiser,
-            acceleration=MLJBase.CPUThreads(),
-            loss=Flux.Losses.mse,
-            rng=stable_rng,
-            lambda=-1.0,
-            alpha=-1.0,
-            epochs=-1,
-            batch_size=-1,
-            subset_of_weights=:incorrect,
-            hessian_structure=:incorrect,
-            backend=:incorrect,
-            ret_distr=true,
-        )
 
-        fitresult, cache, _report = MLJBase.fit(model, 0, X, y)
+@testset "Classification" begin
+# Define the model
+flux_model = Chain(
+    Dense(4, 10, relu),
+    Dense(10, 10, relu),
+    Dense(10, 3)
+)
 
-        history = _report.training_losses
-        @test length(history) == model.epochs + 1
+model = LaplaceClassifier(model=flux_model,epochs=50)
 
-        # increase iterations and check update is incremental:
-        model.epochs = model.epochs + 3
-
-        fitresult, cache, _report = @test_logs(
-            (:info, r""), # one line of :info per extra epoch
-            (:info, r""),
-            (:info, r""),
-            MLJBase.update(model, 2, fitresult, cache, X, y)
-        )
-
-        @test :chain in keys(MLJBase.fitted_params(model, fitresult))
-
-        history = _report.training_losses
-        @test length(history) == model.epochs + 1
-
-        yhat = MLJBase.predict(model, fitresult, X)
-
-        # start fresh with small epochs:
-        model = LaplaceRegression(;
-            builder=builder,
-            optimiser=optimiser,
-            epochs=2,
-            acceleration=CPU1(),
-            rng=stable_rng,
-        )
-
-        fitresult, cache, _report = MLJBase.fit(model, 0, X, y)
-
-        # change batch_size and check it performs cold restart:
-        model.batch_size = 2
-        fitresult, cache, _report = @test_logs(
-            (:info, r""), # one line of :info per extra epoch
-            (:info, r""),
-            MLJBase.update(model, 2, fitresult, cache, X, y)
-        )
-
-        # change learning rate and check it does *not* restart:
-        model.optimiser.eta /= 2
-        fitresult, cache, _report = @test_logs(
-            MLJBase.update(model, 2, fitresult, cache, X, y)
-        )
-
-        # set `optimiser_changes_trigger_retraining = true` and change
-        # learning rate and check it does restart:
-        model.optimiser_changes_trigger_retraining = true
-        model.optimiser.eta /= 2
-        @test_logs(
-            (:info, r""), # one line of :info per extra epoch
-            (:info, r""),
-            MLJBase.update(model, 2, fitresult, cache, X, y)
-        )
-
-        return true
-    end
-
-    seed!(1234)
-    N = 300
-    X = MLJBase.table(rand(Float32, N, 4))
-    ycont = 2 * X.x1 - X.x3 + 0.1 * rand(N)
-    builder = MLJFlux.MLP(; hidden=(16, 8), σ=Flux.relu)
-    optimiser = Flux.Optimise.Adam(0.03)
-
-    @test basictest_regression(X, ycont, builder, optimiser, 0.9)
+X, y = @load_iris
+mach = machine(model, X, y)
+MLJBase.fit!(mach)
+Xnew = (sepal_length = [6.4, 7.2, 7.4],
+        sepal_width = [2.8, 3.0, 2.8],
+        petal_length = [5.6, 5.8, 6.1],
+        petal_width = [2.1, 1.6, 1.9],)
+yhat = MLJBase.predict(mach, Xnew) # probabilistic predictions
+predict_mode(mach, Xnew)   # point predictions
+pdf.(yhat, "virginica")    # probabilities for the "verginica" class
+MLJBase.fitted_params(mach)  # fitted params 
+   
 end
